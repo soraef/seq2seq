a627f909067f52a0c7df21e2ac872352c5e330ed
soraef <sorasorasorayama@gmail.com>
2020-04-17 15:27:59 +0900

    ノイズを追加、テストデータとトレーニングデータを分離

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import math 
 import numpy as np
+import random
 import argparse
 
 import torch
@@ -23,6 +24,7 @@ parser.add_argument("--freq",           default=60,  type=int)
 parser.add_argument("--input_len",      default=20,  type=int)
 parser.add_argument("--output_len",     default=20,  type=int)
 parser.add_argument("--batch_size",     default=600, type=int)
+parser.add_argument("--noise",          action="store_true")
 args = parser.parse_args()
 
 # データセット作成用の関数
@@ -31,14 +33,21 @@ def sin_list(data_num, offset=0, freq=60):
 
 # データは[時系列数, バッチサイズ, 1データの次元数]という形
 # 今回は20点のsin波の入力から次の20点のsin波を予測するので[20, batch_size, 1]という形になる
-def mk_dataset(input_len=20, output_len=20, freq=60, batch_size=600):
+def mk_dataset(input_len=20, output_len=20, freq=60, batch_size=600, noise=False):
     xs = []
     ys = []
     for i in range(batch_size):
         # (input_len + output_len)個の正弦波の点を求めておいて分割する
         wave = sin_list(input_len + output_len, i, freq=freq)
-        x = np.array(wave[:input_len]).astype(np.float32)[:,np.newaxis, np.newaxis]
-        y = np.array([0] + wave[input_len:]).astype(np.float32)[:,np.newaxis, np.newaxis]
+
+        input_wave  = wave[:input_len]
+        output_wave = [0] + wave[input_len:]
+
+        if noise:
+            input_wave = [x + random.uniform(-0.5, 0.5) for x in input_wave]
+
+        x = np.array(input_wave).astype(np.float32)[:,np.newaxis, np.newaxis]
+        y = np.array(output_wave).astype(np.float32)[:,np.newaxis, np.newaxis]
 
         xs.append(x)
         ys.append(y)
@@ -52,16 +61,21 @@ def mk_dataset(input_len=20, output_len=20, freq=60, batch_size=600):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cpu = torch.device("cpu")
 
-# 訓練データを準備
+# データを準備
 INPUT_LEN  = args.input_len
 OUTPUT_LEN = args.output_len
 FREQ       = args.freq
 BATCH_SIZE = args.batch_size
+NOISE      = args.noise
 
-train_x, train_y = mk_dataset(input_len=INPUT_LEN, output_len=OUTPUT_LEN, freq=FREQ, batch_size=BATCH_SIZE)
+train_x, train_y = mk_dataset(input_len=INPUT_LEN, output_len=OUTPUT_LEN, freq=FREQ, batch_size=BATCH_SIZE, noise=NOISE)
+test_x, test_y   = mk_dataset(input_len=INPUT_LEN, output_len=OUTPUT_LEN, freq=FREQ, batch_size=1, noise=NOISE)
 
 train_x = torch.from_numpy(train_x).to(device)
 train_y = torch.from_numpy(train_y).to(device)
+
+test_x = torch.from_numpy(test_x).to(device)
+test_y = torch.from_numpy(test_y).to(device)
 
 # モデルの準備
 INPUT_DIM  = 1
@@ -114,8 +128,8 @@ for epoch in range(args.epochs):
 # 今回はtrain_xの1番目のバッチをtestデータとして使う
 model.eval() # model.eval()を設定するとdropoutを無効化してくれる
 
-test_x = train_x[:, 1:2, :]
-test_y = train_y[:, 1:2, :]
+# test_x = train_x[:, 1:2, :]
+# test_y = train_y[:, 1:2, :]
 
 with torch.no_grad():
     outputs = model(test_x, test_y, 0)
