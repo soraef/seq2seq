fb421cd981c45857cebe5c6e8d40b89548bda0f0
root <root@LAPTOP-BG0S4893.localdomain>
2020-04-17 13:50:19 +0900

    init

---
+++ model.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import glob
+import json
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+
+import random
+import math
+import time
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_layers=1, dropout=0.5):
+        super().__init__()
+
+        self.hidden_dim = hidden_dim
+        self.rnn = nn.LSTM(input_dim, hidden_dim, n_layers)
+        self.dropout = nn.Dropout(dropout)
+    
+    # xs:  入力シーケンス[xs_len, batch_size, x_size]
+    def forward(self, xs):
+        xs = self.dropout(xs)
+        outputs, (hidden, cell) = self.rnn(xs)
+        return hidden, cell
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, input_dim, hidden_dim, n_layers=1, dropout=0.5):
+        super().__init__()
+
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+
+        self.rnn = nn.LSTM(input_dim, hidden_dim, n_layers)
+        self.dropout = nn.Dropout(dropout)
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x, hidden, cell):
+
+        # x = [batch_size, x_size] => [1, batch_size, x_size]
+        x = x.unsqueeze(0)
+
+        x = self.dropout(x)
+
+        output, (hidden, cell) = self.rnn(x, (hidden, cell))
+
+        # output = [1, batch_size, hidden_dim] => [batch_size, hidden_dim]
+        prediction = self.fc_out(output.squeeze(0))
+
+        return prediction, hidden, cell
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device  = device
+
+    # xs: 入力データシーケンス[xs_len, batch_size, x_size]
+    # ys: 教師データシーケンス[ys_len, batch_size, y_size]
+    def forward(self, xs, ys, teacher_forcing_ration = 0.5):
+        batch_size = ys.shape[1]
+        ys_len     = ys.shape[0]
+        y_size     = ys.shape[2]
+
+        outputs = torch.zeros(ys_len, batch_size, y_size).to(self.device)
+
+        hidden, cell = self.encoder(xs)
+
+        y = ys[0, :, :]
+
+        for t in range(1, ys_len):
+            output, hidden, cell = self.decoder(y, hidden, cell)
+            outputs[t] = output
+
+            teacher_force = random.random() < teacher_forcing_ration
+
+            y = ys[t] if teacher_force else output
+
+        return outputs
+
+
---
+++ train.py
@@ -0,0 +1,122 @@
+import math 
+import numpy as np
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from model import Seq2Seq, Encoder, Decoder
+
+parser = argparse.ArgumentParser()
+parser.add_argument("output_file_name", default="result.png")
+parser.add_argument("--hidden_dim", type=int, default=64)
+parser.add_argument("--n_layers", type=int, default=3)
+parser.add_argument("--dropout_rate", type=float, default=0.5)
+parser.add_argument("--epochs", type=int, default=100)
+parser.add_argument("--optimizer", default="adam", choices=["adam", "sgd"])
+args = parser.parse_args()
+
+# データセット作成用の関数
+def sin_list(data_num, offset=0, freq=60):
+    return [math.sin(2 * math.pi * i / freq) for i in range(data_num)]
+
+# データは[時系列数, バッチサイズ, 1データの次元数]という形
+# 今回は20点のsin波の入力から次の20点のsin波を予測するので[20, batch_size, 1]という形になる
+def mk_dataset():
+    batch_size = 600
+    xs = []
+    ys = []
+    for i in range(batch_size):
+        wave = sin_list(40, i)
+        x = np.array(wave[:20]).astype(np.float32)[:,np.newaxis, np.newaxis]
+        y = np.array([0] + wave[20:]).astype(np.float32)[:,np.newaxis, np.newaxis]
+
+        xs.append(x)
+        ys.append(y)
+
+    train_x = np.concatenate(xs, axis=1)
+    train_y = np.concatenate(ys, axis=1)
+
+    return train_x, train_y
+
+# gpuの設定
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+cpu = torch.device("cpu")
+
+# 訓練データを準備
+train_x, train_y = mk_dataset()
+
+train_x = torch.from_numpy(train_x).to(device)
+train_y = torch.from_numpy(train_y).to(device)
+
+# モデルの準備
+INPUT_DIM  = 1
+OUTPUT_DIM = 1
+N_LAYERS   = args.n_layers
+HID_DIM    = args.hidden_dim
+DROPOUT_RATE = args.dropout_rate
+
+enc = Encoder(INPUT_DIM, HID_DIM, N_LAYERS, DROPOUT_RATE)
+dec = Decoder(OUTPUT_DIM, INPUT_DIM, HID_DIM, N_LAYERS, DROPOUT_RATE)
+model = Seq2Seq(enc, dec, device).to(device)
+
+# モデルのパラメータの初期化
+def init_weights(m):
+    for name, param in m.named_parameters():
+        nn.init.uniform_(param.data, -0.08, 0.08)
+
+print(model.apply(init_weights))
+
+# optimizerの設定
+if args.optimizer == "sgd":
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+else:
+    optimizer = optim.Adam(model.parameters())
+
+# 損失関数の設定
+loss_fn = torch.nn.MSELoss()
+
+
+# train
+model.train()
+
+for epoch in range(args.epochs):
+    optimizer.zero_grad()
+
+    outputs = model(train_x, train_y)
+
+    # train_y, outputsの先頭に0が入っているので取り除く
+    train_y1 = train_y[1:]
+    outputs  = outputs[1:]
+
+    loss = loss_fn(train_y1, outputs)
+    loss.backward()
+
+    optimizer.step()
+    if epoch % 10 == 0:
+        print(loss.item())
+
+# test
+# 今回はtrain_xの1番目のバッチをtestデータとして使う
+model.eval() # model.eval()を設定するとdropoutを無効化してくれる
+
+test_x = train_x[:, 1:2, :]
+test_y = train_y[:, 1:2, :]
+
+with torch.no_grad():
+    outputs = model(test_x, test_y, 0)
+
+# 結果の出力
+x = test_x[:, 0, 0].to(cpu).detach().tolist()
+y = outputs[:, 0, 0].to(cpu).detach().tolist()[1:]
+
+plt.plot(list(range(20)), x, color="r", label="train_x")
+plt.plot(list(range(19, 40)), [x[-1]] + y, color="b", label="predict_y")
+plt.savefig("result/" + args.output_file_name)
+
+
